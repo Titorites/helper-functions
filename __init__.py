84ebc62bdcfa70031809b2590b4bde11638f12eb
Titorites <hg2741@columbia.edu>
2025-02-22 19:09:55 -0500

    style: fix flake8

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
-from .helper_functions_package import plot_linreg_package, train_linreg_package
+from .helper_functions_package import plot_linreg_package  # noqa: F401
+from .helper_functions_package import train_linreg_package  # noqa: F401
